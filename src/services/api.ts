f9d7c058978d8f30171ac71f1a989a1e032d04ab
ToluwaDavid <32324558+ToluwaDavid@users.noreply.github.com>
2025-06-21 11:23:07 +0100

    Update api.ts with new render server URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://kaatinserver-production.up.railway.app/api",
+  // baseURL: "https://kaatinserver-production.up.railway.app/api",
+  baseURL: "https://kaatinserver.onrender.com/api",
   withCredentials: true,
 });
 
